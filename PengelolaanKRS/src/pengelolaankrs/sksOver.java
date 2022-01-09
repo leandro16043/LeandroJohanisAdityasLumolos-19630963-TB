6ac7c32f4828c1914e4cb98abaa6191b639b16c3
leandro16043 <81970231+leandro16043@users.noreply.github.com>
2022-01-09 14:46:02 +0700

    Add files via upload

---
+++ PengelolaanKRS/src/pengelolaankrs/sksOver.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pengelolaankrs;
+
+/**
+ *
+ * @author Andro
+ */
+public class sksOver extends Exception{
+    public sksOver()
+    {
+        
+    }
+
+    
+}
